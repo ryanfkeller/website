31362a7313e0bd79864e2181ede30d0d7c3e01b7
Ryan Keller <rkellerf@gmail.com>
2026-06-21 14:10:41 -0700

    testing updates

--- github-actions/utils/check-team-membership.js
+++ github-actions/utils/check-team-membership.js
@@ -9,7 +9,8 @@
 async function isMemberOfTeam(github, context, username, team) {
   try {
     await github.rest.teams.getMembershipForUserInOrg({
-      org: context.repo.owner,
+      // for testing
+      org: 'hackforla',
       team_slug: team,
       username: username,
     });
